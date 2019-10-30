75000656d0b6414490b3223d55a985e3350eb2c8
MilanBojic1999 <mbojic12@gmail.com>
2019-10-30 18:01:51 +0100

    IDDFS bug fixed
    Now IDDFS should work fine

--- Model/Search/IterativeDeepeningDFS.java
+++ Model/Search/IterativeDeepeningDFS.java
@@ -39,9 +39,12 @@ public class IterativeDeepeningDFS extends Search {
             for(Edge edge:g.adj(v)) {
                 int w = edge.other(v);
                 if(!marked[w]) {
+                    System.out.print(w+":");
+                    printOfList(queue);
                     queue.add(w);
                     controler.setNodeState(w,NodeStates.CHECKING);
                     edgeTo[w]=v;
+                    sleep(400);
                     if(w==e)
                         return edgeTo(g,edgeTo,s,e);
                 }
@@ -50,7 +53,7 @@ public class IterativeDeepeningDFS extends Search {
             for(Edge edge:g.adj(v)){
                 int w=edge.other(v);
                 if(!marked[w]){
-                    queue.add(_rDfs(g,queue,marked,edgeTo,w,s,e,1));
+                    _rDfs(g,queue,marked,edgeTo,w,s,e,1);
                     sleep(500);
 //                    if(list!=null)
 //                        return list;
@@ -60,12 +63,13 @@ public class IterativeDeepeningDFS extends Search {
         return edgeTo(g,edgeTo,s,e);
     }
 
-    private int _rDfs(Graph g,Queue<Integer> queue,boolean[] marked,int[] edgeTo,int v,int s,int e,int i){
+    private void  _rDfs(Graph g,Queue<Integer> queue,boolean[] marked,int[] edgeTo,int v,int s,int e,int i){
         if(i==4){
             marked[v]=true;
+            queue.add(v);
             NodeControler.getInstance().setNodeState(v,NodeStates.TEST);
             sleep(400);
-            return v;
+            return;
         }
         marked[v]=true;
         NodeControler.getInstance().setNodeState(v,NodeStates.CHECKED);
@@ -78,13 +82,13 @@ public class IterativeDeepeningDFS extends Search {
                 sleep(500);
                 if(w==e) {
                     edgeTo[w]=v;
-                    return w;
+                    queue.add(w);
+                    return;
                 }
                 else
-                    return _rDfs(g,queue,marked,edgeTo,w,s,e,i+1);
+                    _rDfs(g,queue,marked,edgeTo,w,s,e,i+1);
             }
         }
-        return v;
     }
 
     @Override
